15b9fef9eaaefc51346a68eff0858ada404c956d
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2021-12-04 11:04:02 -0600

    moved setting textContent of current section to new function; added ability to switch between C and F

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var cityInputEl = document.getElementById("location");
 var searchFormEl = document.getElementById("location-form");
 // main section elements
 var cityNameEl = document.getElementById("city-name");
-var CurrDateEl = document.getElementById("current-date");
+var currDateEl = document.getElementById("current-date");
 var tempEl = document.getElementById("temp");
 var windEl = document.getElementById("wind");
 var humidityEl = document.getElementById("humidity");
@@ -28,8 +28,6 @@ function citySearch(cityInput) {
                     response.json().then(function (data) {
                         const lat = data[0].lat;
                         const lon = data[0].lon;
-                        console.log(lat);
-                        console.log(lon);
                         //  store standardized name from api for display
                         const loc = data[0].name;
                         getWeatherData(lat, lon, loc)
@@ -47,18 +45,22 @@ function citySearch(cityInput) {
 }
 
 function getWeatherData(lat, lon, loc) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=metric&appid=858d2ac1d226880ff65be3ab6336fd05"
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=" + unitType + "&appid=858d2ac1d226880ff65be3ab6336fd05"
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
                     // display returned info on screen
-                    cityNameEl.textContent = loc
-                    CurrDateEl.textContent = moment().format("dddd, MMMM DD, YYYY");
-                    tempEl.textContent = data.current.temp;
-                    windEl.textContent = data.current.wind;
-                    humidityEl.textContent = data.current.humidity;
-                    uvIndexEl.textContent = data.current.uvi;
+                    var current = [{loc: loc, temp: Math.round(data.current.temp), wind: data.current.wind_speed, humidity: data.current.humidity, uvi: data.current.uvi}];
+                    displayCurrent(current);
+                    // cityNameEl.textContent = loc
+                    // currDateEl.textContent = moment().format("dddd, MMMM D, YYYY");
+                    // tempEl.textContent = Math.round(data.current.temp);
+                    // windEl.textContent = data.current.wind_speed;
+                    // humidityEl.textContent = data.current.humidity;
+                    // uvIndexEl.textContent = data.current.uvi;
+                    var forecast = [];
+                    displayForecast(forecast);
                 });
             } else {
                 alert("Error: " + response.statusText);
@@ -69,9 +71,38 @@ function getWeatherData(lat, lon, loc) {
         });
 }
 
+function displayCurrent(current) {
+    // display returned info on 
+    console.log(current);
+    cityNameEl.textContent = current[0].loc;
+    currDateEl.textContent = moment().format("dddd, MMMM D, YYYY");
+    tempEl.textContent = current[0].temp;
+    windEl.textContent = current[0].wind;
+    humidityEl.textContent = current[0].humidity + "%";
+    uvIndexEl.textContent = current[0].uvi;
+    var forecast = [];
+    displayForecast(forecast);
+}
+
+function displayForecast(forecast) {
+
+}
+
 function changeTempUnit() {
     // use to specify query param for units; stored in local storage; imperial is default
-    (unitType = "imperial" ? "metric" : "imperial");
+    if (unitType === "imperial") {
+        unitType = "metric";
+        tempUnitBtn.textContent = "C" 
+    } else {
+        unitType = "imperial";
+        tempUnitBtn.textContent = "F";
+    }
+    // redo current search with new unit
+    var cityInput = cityInputEl.value.trim();
+    console.log(cityInput);
+        if (cityInput != "") {
+            citySearch(cityInput);
+        }
 }
 
 // Event Handlers
